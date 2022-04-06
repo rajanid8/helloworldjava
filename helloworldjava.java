2287778a16fa8bed684b4b076f84644517da4f37
rajanid8 <60307283+rajanid8@users.noreply.github.com>
2022-04-07 01:22:57 +0530

    update

--- helloworldjava.java
+++ helloworldjava.java
@@ -1,6 +1,7 @@
 public class helloworldjava {
    public static void main(String[] args) {
-    System.out.println("Hello World from DevOps");
+    System.out.println("Hello World from DevOps !!");
+      System.out.println("Welcome to Jenkins World");
     }
 }
 
