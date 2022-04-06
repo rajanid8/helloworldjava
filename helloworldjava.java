c1859bede51e5aac911518c829437909ecdfe4db
rajanid8 <60307283+rajanid8@users.noreply.github.com>
2022-04-07 01:58:16 +0530

    update

--- helloworldjava.java
+++ helloworldjava.java
@@ -1,6 +1,6 @@
 public class helloworldjava {
    public static void main(String[] args) {
-      System.out.println("Welcome to Jenkins World !!");
+      System.out.println("Welcome to DevOps-Jenkins World !!");
       System.out.println("Now the Jenkins build is being triggered Automatically by using Webhook !!!!");
     }
 }
